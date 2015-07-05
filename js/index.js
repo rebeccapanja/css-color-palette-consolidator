1df43ddf6e987c2743efebb1c025c9fc94d3b33b
Rebecca Panja <rebeccapanja@gmail.com>
2015-07-04 22:34:37 -0400

    Add bootstrap and jquery

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
 function display(collection, parent) {
-    var out = [];
-    var size = 10;
-    for (var i=0,l=collection.length; i<l; i++) {
-        size = (Math.min(50, collection[i].count) * 2) + "px";
-        out.push("<span class='color' style='background:"+collection[i].original+"; width:"+size+"' title='"+collection[i].original+' : '+collection[i].count+"'><em>"+collection[i].original+"</em> </span>")
-    }
-    parent.innerHTML = out.join("");
+  var out = [];
+  var size = 10;
+  for (var i=0,l=collection.length; i<l; i++) {
+    size = (Math.min(50, collection[i].count) * 2) + "px";
+    out.push("<span class='color' style='background:"+collection[i].original+";width:"+size+"' title='"+collection[i].original+' : '+collection[i].count+"'><em>"+collection[i].original+"</em></span>");
+  }
+  parent.html(out.join(""));
 }
 
 function extract() {
-    var cssText = document.getElementById("source").value;
-    Palette.init(cssText);
-    
-    display(Palette.bw, document.getElementById("bw"));
-    display(Palette.colors, document.getElementById("colors"));
+  var cssText = $('#source').val();
+  Palette.init(cssText);
+  display(Palette.bw, $('#bw'));
+  display(Palette.colors, $('#colors'));
 }
+
+$(function() {
+  $('.extract').click(function(){
+    extract();
+  });
+});
